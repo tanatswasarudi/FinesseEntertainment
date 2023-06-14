9dc828f91ce7e7e6ca8c57c7931c88d5ef6d7375
tanatswasarudi <tanatswasarudi09@gmail.com>
2023-06-15 01:53:40 +0530

    long code

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -7,7 +7,8 @@ import {  BsFacebook } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 import { FaWhatsappSquare } from "react-icons/fa";
 import {BsFillSpeakerFill} from 'react-icons/bs'
-
+import { Link } from "react-router-dom";
+import {IoMdCall} from 'react-icons/io'
 
 
 const About = () => {
@@ -24,6 +25,13 @@ const About = () => {
     // Open the WhatsApp URL
     window.open(url);
   }
+  const phoneNumber = '+263 783-677-124'; // Replace with the desired recipient's phone number
+
+  const handleCallRequest = () => {
+    const message = 'Please call me back!'; // Customize the call request message
+    const callUrl = `tel:${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(callUrl);
+  };
   
 
   return (
@@ -36,22 +44,18 @@ const About = () => {
   </div>
   <h1 className="text-white text-lg md:px-8 font-serif mt-2 mb-6">
           We are here to provide the best service <BsFillSpeakerFill className='text-2xl text-red-500'/>
-          </h1>
-          <p className="text-gray-500 md:px-10 ">
-              <span className="flex flex-row items-center cursor-pointer font-serif hover:text-green-900"><MdEmail/><strong>Email:</strong>finesseentertainment@gmail.com</span><br/>
-              <span className="flex flex-row items-center cursor-pointer font-serif hover:text-blue-500"><BsFacebook className="text-blue-500" /><strong>Facebook: </strong>FinesseEntertainment</span><br/>
-              <span className="flex flex-row items-center cursor-pointer font-serif hover:text-green-900" onClick={connectWithWhatsApp}><FaWhatsappSquare className="text-green-500" /><strong>WhatsApp:</strong>+918264420815</span><br/>
-              </p>
+          </h1>     
 
-     <div className="py-10 md:px-8 grid grid-cols-1 lg:grid-cols-2">
+     <div className="py-10 md:px-8 grid grid-cols-1 md:grid-cols-2 justify-center items-center w-full ">
        <div className="text-cyan-500 cursor-pointer  ">
+        <Link to={""}>
             <h1 className="bg-slate-200 rounded shadow text-center md:text-3xl text-base font-bold">
             OUR BEST OFFERS  <span> </span>
            
           </h1>
          
-          
-          <h3 className="bg-slate-200 font-serif mt-2  rounded hover:text-green-500 shadow cursor-pointer">
+          </Link>
+          <h3 className="bg-slate-200 font-serif  mb-6  rounded hover:bg-slate-500 shadow cursor-pointer">
           <span className="text-cyan-500 mb-6"> Connect With Us</span> 
           <span className="flex flex-row items-center  font-serif " onClick={connectWithWhatsApp}><FaWhatsappSquare className="text-3xl text-green-500"/></span>
             
@@ -59,8 +63,18 @@ const About = () => {
         
 
         </div>
+        <div className="px-10">
+        <p className="text-gray-500 md:px-10 ">
+              <span className="flex flex-row items-center cursor-pointer font-serif hover:text-green-900"><MdEmail/><strong>Email:</strong>finesseentertainment@gmail.com</span><br/>
+              <span className="flex flex-row items-center cursor-pointer font-serif hover:text-blue-500"><BsFacebook className="text-blue-500" /><strong>Facebook: </strong>FinesseEntertainment</span><br/>
+              <ul>
+              <li className="flex flex-row items-center font-serif hover:text-yellow-500 text-gray-500" onClick={handleCallRequest}><IoMdCall className="text-blue-500"/> <strong>Phone:</strong>+263 783-677-124</li>
+              </ul>
+              </p>
+        </div>
+        </div>
            
-    <div className='flex flex-col gap-5 py-10 overflow-scroll scrollbar-none scroll-smooth' >
+    <div className='flex flex-col justify-center items-center gap-5 py-20 overflow-scroll scrollbar-none scroll-smooth' >
           {
             homeProductCartListChairs.map(el=>{
               return(
@@ -78,7 +92,7 @@ const About = () => {
            }
           
         </div>
-    </div>
+    
    
 
     </div>
